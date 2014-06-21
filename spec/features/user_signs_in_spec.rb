667cea4ed7af370944c1543efaeb6e7a04cd6ff7
Vasco Silva <vascosilva@cosamalamac.home>
2014-06-21 17:11:51 +0100

    - Fixed warnings made bu Hound;

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-feature 'User signs in' do
+  feature 'User signs in' do
     scenario 'success login' do
-        user = User.create email: "vasco@gmail.com", password: "iasdodioas"
-        visit sign_in_path
+      user = User.create email: 'vasco@gmail.com', password: 'iasdodioas'
+      visit sign_in_path
 
-        within 'form' do
-            fill_in 'Email', with: user.email
-            fill_in 'Password', with: "iasdodioas"
+      within 'form' do
+          fill_in 'Email', with: user.email
+          fill_in 'Password', with: 'iasdodioas'
 
-            click_on 'Sign in'
-        end
+          click_on 'Sign in'
+      end
 
-        expect(page).to have_content 'Welcome'
-    end 
+      expect(page).to have_content 'Welcome'
+  end 
 end
