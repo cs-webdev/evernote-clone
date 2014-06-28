5a65123a9b98c9c3482e7f84427fd2e64ee32852
Miguel Fernandes <migpfernandes@gmail.com>
2014-06-28 16:07:58 +0100

    Fixed formatting errors reported by hound.

--- spec/features/guest_view_notebooks_spec.rb
+++ spec/features/guest_view_notebooks_spec.rb
@@ -1,58 +1,58 @@
 require 'rails_helper'
 
 feature 'Guest view notebooks' do
-	scenario 'view all notebooks' do
-		Notebook.create description: 'First Notebook'
-		Notebook.create description: 'Second Notebook'
-		visit root_path
+  scenario 'view all notebooks' do
+    Notebook.create description: 'First Notebook'
+    Notebook.create description: 'Second Notebook'
 
-		expect(page).to have_text 'First Notebook'
-		expect(page).to have_text 'Second Notebook'
-	end
+    visit root_path
 
-	scenario 'view notebook' do
-		notebook = Notebook.create description: 'First Notebook'
-		Notebook.create description: 'Second Notebook'
+    expect(page).to have_text 'First Notebook'
+    expect(page).to have_text 'Second Notebook'
+  end
 
-		visit notebook_path(notebook)
+  scenario 'view notebook' do
+    notebook = Notebook.create description: 'First Notebook'
+    Notebook.create description: 'Second Notebook'
 
-		expect(page).to have_text 'First Notebook'
-		expect(page).not_to have_text 'Second Notebook'
-	end
+    visit notebook_path(notebook)
 
-	scenario 'create notebook' do
-		visit new_notebook_path
+    expect(page).to have_text 'First Notebook'
+    expect(page).not_to have_text 'Second Notebook'
+  end
 
-		within 'form' do
-			fill_in 'Description', with: 'xpto'
+  scenario 'create notebook' do
+    visit new_notebook_path
+
+    within 'form' do
+      fill_in 'Description', with: 'xpto'
 
       click_on 'Create Notebook'
     end
+    expect(page).to have_text 'xpto'
+  end
 
-		expect(page).to have_text 'xpto'
-	end
+  scenario 'delete notebook' do
+    notebook = Notebook.create description: 'First Notebook'
+    visit notebooks_path
 
-	scenario 'delete notebook' do
-		notebook = Notebook.create description: 'First Notebook'
-		visit notebooks_path
+    click_on 'Delete'
 
-		click_on 'Delete'
+    expect(page).not_to have_text('First Notebook')
+  end
 
-		expect(page).not_to have_text('First Notebook')
-	end
+  scenario 'edit notebook' do
+    notebook = Notebook.create description: 'First Notebook'
+    visit edit_notebook_path(notebook)
 
-	scenario 'edit notebook' do
-		notebook = Notebook.create description: 'First Notebook'
-		visit edit_notebook_path(notebook)
-
-		within 'form' do
-			fill_in 'Description', with: 'xpto'
+    within 'form' do
+      fill_in 'Description', with: 'xpto'
 
       click_on 'Update Notebook'
     end
 
-		expect(page).not_to have_text('First Notebook')
-		expect(page).to have_text('xpto')
-	end
+    expect(page).not_to have_text('First Notebook')
+    expect(page).to have_text('xpto')
+  end
 
 end
