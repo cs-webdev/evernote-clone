fe5fe82c2316ecce4e3bc939886b8177d2f94426
Miguel Fernandes <migpfernandes@gmail.com>
2014-07-01 01:19:04 +0100

    Adicionada relação entre users e notebooks

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140621113026) do
+ActiveRecord::Schema.define(version: 20140701001524) do
 
   create_table "notebooks", force: true do |t|
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "notebooks", ["user_id"], name: "index_notebooks_on_user_id"
 
   create_table "users", force: true do |t|
     t.datetime "created_at",                     null: false
--- spec/features/guest_view_notebooks_spec.rb
+++ spec/features/guest_view_notebooks_spec.rb
@@ -11,6 +11,26 @@ feature "Guest view notebooks" do
     expect(page).to have_text "Second Notebook"
   end
 
+  scenario "guest can only see his notebooks" do
+    user1 = User.create email: 'vasco@gmail.com', password: 'iasdodioas'
+    user2 = User.create email: 'migpfernandes@gmail.com', password: 'iasdodioas'
+
+    Notebook.create description: "First Notebook" user: user1
+    Notebook.create description: "Second Notebook" user: user2
+
+    visit sign_in_path
+
+    within 'form' do
+      fill_in 'Email', with: user1.email
+      fill_in 'Password', with: 'iasdodioas'
+
+      click_on 'Sign in'
+    end
+
+    expect(page).to have_text "First Notebook"
+    expect(page).not_to have_text "Second Notebook"
+  end
+
   scenario "view notebook" do
     notebook = Notebook.create description: "First Notebook"
     Notebook.create description: "Second Notebook"
